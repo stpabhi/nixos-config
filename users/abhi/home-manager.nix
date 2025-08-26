1baf93eae268de387aa7420b127a9279ca57fa1f
Abhilash Pallerlamudi <stp.abhi@gmail.com>
2025-08-25 22:43:04 -0700

    add direnv

--- users/abhi/home-manager.nix
+++ users/abhi/home-manager.nix
@@ -77,18 +77,10 @@ in {
     shellOptions = [];
     historyControl = [ "ignoredups" "ignorespace" ];
     initExtra = builtins.readFile ./bashrc;
+  };
 
-    shellAliases = {
-      ga = "git add";
-      gc = "git commit";
-      gco = "git checkout";
-      gcp = "git cherry-pick";
-      gdiff = "git diff";
-      gl = "git prettylog";
-      gp = "git push";
-      gs = "git status";
-      gt = "git tag";
-    };
+  programs.direnv= {
+    enable = true;
   };
 
   programs.zsh = {
